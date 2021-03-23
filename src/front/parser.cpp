a9f9be8936f8e106df7a84cb907fbea2080e14c1
MaxXSoft <x@MaxXSoft.net>
2021-03-23 17:37:53 +0800

    (parser) added the implementation of parser

---
+++ src/front/parser.cpp
@@ -0,0 +1,260 @@
+#include "front/parser.h"
+
+#include <iostream>
+#include <algorithm>
+
+namespace {
+
+//
+
+}  // namespace
+
+ASTPtr Parser::LogError(std::string_view message) {
+  std::cerr << "error(parser): " << message << std::endl;
+  ++error_num_;
+  return nullptr;
+}
+
+ASTPtr Parser::ParseFunDef() {
+  // get function name
+  if (!ExpectId()) return nullptr;
+  auto name = lexer_.id_val();
+  NextToken();
+  // check & eat '('
+  if (!ExpectChar('(')) return nullptr;
+  // get formal arguments
+  IdList args;
+  if (!IsTokenChar(')')) {
+    for (;;) {
+      // get name of the current argument
+      if (!ExpectId()) return nullptr;
+      args.push_back(lexer_.id_val());
+      NextToken();
+      // eat ','
+      if (!IsTokenChar(',')) break;
+      NextToken();
+    }
+  }
+  // check & eat ')'
+  if (!ExpectChar(')')) return nullptr;
+  // get function body
+  auto body = ParseBlock();
+  if (!body) return nullptr;
+  return std::make_unique<FunDefAST>(name, std::move(args),
+                                     std::move(body));
+}
+
+ASTPtr Parser::ParseBlock() {
+  // check & eat '{'
+  if (!ExpectChar('{')) return nullptr;
+  // get statements
+  ASTPtrList stmts;
+  while (!IsTokenChar('}')) {
+    auto stmt = ParseStatement();
+    if (!stmt) return nullptr;
+    stmts.push_back(std::move(stmt));
+  }
+  // eat '}'
+  NextToken();
+  return std::make_unique<BlockAST>(std::move(stmts));
+}
+
+ASTPtr Parser::ParseStatement() {
+  switch (cur_token_) {
+    case Token::Id: return ParseAssign();
+    case Token::Keyword: {
+      if (lexer_.key_val() == Keyword::If) {
+        return ParseIfElse();
+      }
+      else if (lexer_.key_val() == Keyword::Return) {
+        return ParseReturn();
+      }
+      // fallthrough
+    }
+  }
+  return LogError("invalid statement");
+}
+
+ASTPtr Parser::ParseAssign() {
+  // get name of variable
+  auto name = lexer_.id_val();
+  NextToken();
+  // get expression
+  auto expr = ParseExpr();
+  if (!expr) return nullptr;
+  return std::make_unique<AssignAST>(name, std::move(expr));
+}
+
+ASTPtr Parser::ParseIfElse() {
+  // eat 'if'
+  NextToken();
+  // get condition
+  auto cond = ParseExpr();
+  if (!cond) return nullptr;
+  // get 'then' body
+  auto then = ParseBlock();
+  if (!then) return nullptr;
+  // get 'else-then' body
+  ASTPtr else_then;
+  if (IsTokenKey(Keyword::Else)) {
+    // eat 'else'
+    NextToken();
+    else_then = IsTokenKey(Keyword::If) ? ParseIfElse() : ParseBlock();
+    if (!else_then) return nullptr;
+  }
+  return std::make_unique<IfAST>(std::move(cond), std::move(then),
+                                 std::move(else_then));
+}
+
+ASTPtr Parser::ParseReturn() {
+  // eat 'return'
+  NextToken();
+  // get return value
+  auto expr = ParseExpr();
+  if (!expr) return nullptr;
+  return std::make_unique<ReturnAST>(std::move(expr));
+}
+
+ASTPtr Parser::ParseExpr() {
+  return ParseBinary([this] { return ParseLAndExpr(); }, {Operator::LOr});
+}
+
+ASTPtr Parser::ParseLAndExpr() {
+  return ParseBinary([this] { return ParseEqExpr(); }, {Operator::LAnd});
+}
+
+ASTPtr Parser::ParseEqExpr() {
+  return ParseBinary([this] { return ParseRelExpr(); },
+                     {Operator::Eq, Operator::NotEq});
+}
+
+ASTPtr Parser::ParseRelExpr() {
+  return ParseBinary([this] { return ParseAddExpr(); },
+                     {Operator::Less, Operator::LessEq});
+}
+
+ASTPtr Parser::ParseAddExpr() {
+  return ParseBinary([this] { return ParseMulExpr(); },
+                     {Operator::Add, Operator::Sub});
+}
+
+ASTPtr Parser::ParseMulExpr() {
+  return ParseBinary([this] { return ParseUnaryExpr(); },
+                     {Operator::Mul, Operator::Div, Operator::Mod});
+}
+
+ASTPtr Parser::ParseUnaryExpr() {
+  if (cur_token_ == Token::Operator) {
+    // get operator
+    auto op = lexer_.op_val();
+    NextToken();
+    // check if is a valid operator
+    switch (op) {
+      case Operator::Sub: case Operator::LNot: break;
+      default: return LogError("invalid unary operator");
+    }
+    // get operand
+    auto opr = ParseValue();
+    if (!opr) return nullptr;
+    return std::make_unique<UnaryAST>(op, std::move(opr));
+  }
+  else {
+    return ParseValue();
+  }
+}
+
+ASTPtr Parser::ParseValue() {
+  switch (cur_token_) {
+    case Token::Integer: {
+      // get integer literal
+      auto val = lexer_.int_val();
+      NextToken();
+      return std::make_unique<IntAST>(val);
+    }
+    case Token::Id: {
+      // get identifier
+      auto id = lexer_.id_val();
+      NextToken();
+      // check if the current token is '('
+      return IsTokenChar('(') ? ParseFunCall()
+                              : std::make_unique<IdAST>(id);
+    }
+    case Token::Other: {
+      if (lexer_.other_val() == '(') {
+        // eat '('
+        NextToken();
+        // get expression
+        auto expr = ParseExpr();
+        if (!expr) return nullptr;
+        // check & eat ')'
+        ExpectChar(')');
+        return expr;
+      }
+      // fallthrough
+    }
+  }
+  return LogError("invalid value");
+}
+
+ASTPtr Parser::ParseFunCall() {
+  // get function name
+  auto name = lexer_.id_val();
+  // eat '('
+  NextToken();
+  // get arguments
+  ASTPtrList args;
+  if (!IsTokenChar(')')) {
+    for (;;) {
+      // get the current argument
+      auto expr = ParseExpr();
+      if (!expr) return nullptr;
+      args.push_back(std::move(expr));
+      NextToken();
+      // eat ','
+      if (!IsTokenChar(',')) break;
+      NextToken();
+    }
+  }
+  // check & eat ')'
+  if (!ExpectChar(')')) return nullptr;
+  return std::make_unique<FunCallAST>(name, std::move(args));
+}
+
+ASTPtr Parser::ParseBinary(std::function<ASTPtr()> parser,
+                           std::initializer_list<Operator> ops) {
+  // get left-hand side expression
+  auto lhs = parser();
+  if (!lhs) return nullptr;
+  // get the rest things
+  while (cur_token_ == Token::Operator &&
+         std::find(ops.begin(), ops.end(), lexer_.op_val()) != ops.end()) {
+    // get operator
+    auto op = lexer_.op_val();
+    NextToken();
+    // get right-hand side expression
+    auto rhs = parser();
+    if (!rhs) return nullptr;
+    // update lhs
+    lhs = std::make_unique<BinaryAST>(op, std::move(lhs), std::move(rhs));
+  }
+  return lhs;
+}
+
+bool Parser::ExpectId() {
+  if (cur_token_ != Token::Id) {
+    LogError("expected identifier");
+    return false;
+  }
+  return true;
+}
+
+bool Parser::ExpectChar(char c) {
+  if (!IsTokenChar(c)) {
+    std::string msg = "expected '";
+    msg = msg + c + "'";
+    LogError(msg);
+    return false;
+  }
+  NextToken();
+  return true;
+}
---
+++ src/front/parser.h
@@ -0,0 +1,71 @@
+#ifndef FIRSTSTEP_FRONT_PARSER_H_
+#define FIRSTSTEP_FRONT_PARSER_H_
+
+#include <string_view>
+#include <functional>
+#include <initializer_list>
+#include <cstddef>
+
+#include "front/lexer.h"
+#include "define/ast.h"
+#include "define/token.h"
+
+class Parser {
+ public:
+  Parser(Lexer &lexer) : lexer_(lexer) {
+    error_num_ = 0;
+    NextToken();
+  }
+
+  // parse next symbol
+  ASTPtr ParseNext() {
+    return cur_token_ == Token::End ? nullptr : ParseFunDef();
+  }
+
+  // count of error
+  std::size_t error_num() const { return error_num_; }
+
+ private:
+  Token NextToken() { return cur_token_ = lexer_.NextToken(); }
+  // check if the current token is the specific character
+  bool IsTokenChar(char c) const {
+    return cur_token_ == Token::Other && lexer_.other_val() == c;
+  }
+  // check if the current token is the specific keyword
+  bool IsTokenKey(Keyword key) const {
+    return cur_token_ == Token::Keyword && lexer_.key_val() == key;
+  }
+
+  // print error message to stderr
+  ASTPtr LogError(std::string_view message);
+
+  ASTPtr ParseFunDef();
+  ASTPtr ParseBlock();
+  ASTPtr ParseStatement();
+  ASTPtr ParseAssign();
+  ASTPtr ParseIfElse();
+  ASTPtr ParseReturn();
+  ASTPtr ParseExpr();
+  ASTPtr ParseLAndExpr();
+  ASTPtr ParseEqExpr();
+  ASTPtr ParseRelExpr();
+  ASTPtr ParseAddExpr();
+  ASTPtr ParseMulExpr();
+  ASTPtr ParseUnaryExpr();
+  ASTPtr ParseValue();
+  ASTPtr ParseFunCall();
+
+  // parse binary expression
+  ASTPtr ParseBinary(std::function<ASTPtr()> parser,
+                     std::initializer_list<Operator> ops);
+  // make sure current token is identifier
+  bool ExpectId();
+  // make sure current token is specific character and goto next token
+  bool ExpectChar(char c);
+
+  Lexer &lexer_;
+  std::size_t error_num_;
+  Token cur_token_;
+};
+
+#endif  // FIRSTSTEP_FRONT_PARSER_H_
