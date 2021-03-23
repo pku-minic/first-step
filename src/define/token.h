f38c4163eb105ee06bf4c84e4bcd86eec84b23aa
MaxXSoft <x@MaxXSoft.net>
2021-03-23 15:08:46 +0800

    (token) added some definitions about token

---
+++ src/define/token.h
@@ -0,0 +1,27 @@
+#ifndef FIRSTSTEP_DEFINE_TOKEN_H_
+#define FIRSTSTEP_DEFINE_TOKEN_H_
+
+// all supported keywords
+#define FIRSTSTEP_KEYWORDS(e) \
+  e(If, "if") e(Else, "else") e(Return, "return")
+
+// all supported operators
+#define FIRSTSTEP_OPERATORS(e) \
+  e(Add, "+") e(Sub, "-") e(Mul, "*") e(Div, "/") e(Mod, "%") \
+  e(Less, "<") e(LessEq, "<=") e(Eq, "==") e(NotEq, "!=") \
+  e(LAnd, "&&") e(LOr, "||") e(LNot, "!") e(Assign, "=")
+
+// expand first element to comma-separated list
+#define FIRSTSTEP_EXPAND_FIRST(i, ...)      i,
+// expand second element to comma-separated list
+#define FIRSTSTEP_EXPAND_SECOND(i, j, ...)  j,
+
+enum class Token {
+  Error, End,
+  Id, Integer, Keyword, Operator, Other,
+};
+
+enum class Keyword { FIRSTSTEP_KEYWORDS(FIRSTSTEP_EXPAND_FIRST) };
+enum class Operator { FIRSTSTEP_OPERATORS(FIRSTSTEP_EXPAND_FIRST) };
+
+#endif  // FIRSTSTEP_DEFINE_TOKEN_H_
