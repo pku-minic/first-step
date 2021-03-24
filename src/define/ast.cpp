7bea509d8cc45fce3d44f14eed3a0d68a4276624
MaxXSoft <x@MaxXSoft.net>
2021-03-24 10:32:36 +0800

    (ast) added some implementations

---
+++ src/define/ast.cpp
@@ -0,0 +1,43 @@
+#include "define/ast.h"
+
+#include "back/interpreter/interpreter.h"
+
+std::optional<int> FunDefAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> BlockAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> AssignAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> IfAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> ReturnAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> BinaryAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> UnaryAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> FunCallAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> IntAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
+
+std::optional<int> IdAST::Eval(Interpreter &intp) const {
+  return intp.EvalOn(*this);
+}
